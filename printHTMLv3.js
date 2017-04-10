4b6e8a70c1350a0d4c961ff3d48c834655414f97
Socha17 <mattsocha11@gmail.com>
2017-04-10 15:47:25 +0000

    added 3rd function kinda modular?

---
+++ printHTMLv3.js
@@ -0,0 +1,44 @@
+var https = require('https');
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step3.html'
+};
+
+function getAndPrintHTML (requestOptions) {
+
+
+
+  // notice that https.get takes a callback with one parameter -
+  // response, which is a Stream that represents the HTTP response
+  https.get(requestOptions, function (response) {
+
+
+
+    // set encoding of received data to UTF-8
+    response.setEncoding('utf8');
+
+    // the callback is invoked when a `data` chunk is received
+    response.on('data', function (data) {
+      for (var i = 0; i < data.length; i++) {
+        var dataReceived = data;
+      }
+      console.log('Data Received:', dataReceived + '\n');
+    });
+
+    // the callback is invoked when all of the data has been received
+    // (the `end` of the stream)
+    response.on('end', function() {
+      console.log('Response stream complete.');
+    });
+
+  });
+
+
+}
+
+
+
+
+
+getAndPrintHTML(requestOptions);
